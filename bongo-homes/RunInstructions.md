49a98a6a85585c5daab89184cb0c4b31fd5160c1
DimoDimitrov <73022213+DimoDimitrov@users.noreply.github.com>
2025-02-04 15:17:22 +0200

    Foundations.
    This PR contains the foundation for the project. Pushing because I will do excercise for the course here and I want to have a clean copy.

---
+++ bongo-homes/RunInstructions.md
@@ -0,0 +1,4 @@
+To run the project:
+    cd bongo-homes
+    npm install
+    npm run dev
---
+++ bongo-homes/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar(){
+    return(
+        <header>
+            <nav className="navbar">
+                <img className="logo" src="public\Images\Downloads\openart-image_hXQoGuZD_1738492286813_raw.jpg" alt="This is the logo" />
+                <h1 className="heading"> Bongo Facts</h1>           
+            </nav>
+        </header>
+    )
+}
